76f16c69386c65018d3c8969718adf88cc2681d0
Lumis08 <limbin234@gmail.com>
2019-12-16 13:03:08 +0800

    Init

---
+++ app/src/main/java/com/example/testinggithub/teset2.kt
@@ -0,0 +1,12 @@
+package com.example.testinggithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class teset2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teset2)
+    }
+}
